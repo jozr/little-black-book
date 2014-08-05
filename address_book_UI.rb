f1bff14639d02fe88b7cb58ba9584b914d4358f9
Aoede Pando & Josephine Wright <student@epicodus.com>
2014-08-05 16:38:26 -0700

    WIP

--- address_book_UI.rb
+++ address_book_UI.rb
@@ -8,11 +8,14 @@ require './lib/phone.rb'
       puts "Press 'c' to create a new contact."
       puts "Press 'x' to exit."
       puts "Press 'a' to add an address"
+      puts "Press 'v' to view a contact"
       main_choice = gets.chomp
       if main_choice == 'c'
         add_contact
       elsif main_choice == 'a'
         add_address
+      elsif main_choice == 'v'
+        view_contact
       elsif main_choice == 'x'
         exit
       end
@@ -33,16 +36,32 @@ require './lib/phone.rb'
       puts looplord.name
     end
 
-    user_input = gets.chomp
-    puts 'Enter the street.'
-    input_street = gets.chomp
-    puts 'Enter the city.'
-    input_city = gets.chomp
-    puts 'Enter the state.'
-    input_state = gets.chomp
-    puts 'Enter the zip code.'
-    input_zip = gets.chomp
-    Address.new(input_street, input_city, input_state, input_zip).save
+    contact_input = gets.chomp
+    Contact.all.each do |looplord|
+      if contact_input = looplord
+        puts 'Enter the street.'
+        input_street = gets.chomp
+        puts 'Enter the city.'
+        input_city = gets.chomp
+        puts 'Enter the state.'
+        input_state = gets.chomp
+        puts 'Enter the zip code.'
+        input_zip = gets.chomp
+        Address.new(input_street, input_city, input_state, input_zip).save
+      end
+      break
+    end
+
+    def view_contact
+      puts "What contact would you like to view?"
+      Contact.all.each do |looplord|
+        puts looplord.name
+        user_input = gets.chomp
+        if looplord == user_input
+          puts 'looplord'
+        end
+      end
+    end
   end
 
   main_menu
